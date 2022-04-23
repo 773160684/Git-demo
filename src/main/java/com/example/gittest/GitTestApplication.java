f2db61c75b431192aeb79a99abf7681a6b3f0fbf
LiuXiaoJie <1985445359@qq.com>
2022-04-23 19:40:50 +0800

    master fouth commit

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -9,9 +9,6 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
         System.out.println("你好");
-        System.out.println("我是master");
-
-        System.out.println("我是hot-fix");
     }
 
 }
