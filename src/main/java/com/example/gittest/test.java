e57d4dc3bbc7ad92abd0b475f747f6781999de2b
Linchuxu <linchuxu2000@qq.com>
2022-04-23 22:33:01 +0800

    chuxu3 commit

--- src/main/java/com/example/gittest/test.java
+++ src/main/java/com/example/gittest/test.java
@@ -2,8 +2,9 @@ package com.example.gittest;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("我来测试了");
-        System.out.println("我来创建了");
+        System.out.println("我来测试了1");
+        System.out.println("我来创建了2");
+        System.out.println("我来创建了3");
 
     }
 }
