4546c723a2bfc4e91be7a75a74647c0bf41331b3
LiuXiaoJie <1985445359@qq.com>
2022-04-23 19:07:28 +0800

    hot-fix second commit

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -9,7 +9,8 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
         System.out.println("你好");
-        System.out.println("我是1");
+        System.out.println("我是hot-fix");
+
     }
 
 }
